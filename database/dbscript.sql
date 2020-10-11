6b11da5d29baa2b903b5587da616115b3a7a0418
Mithul Mangaldas <mithulmangaldas@Mithuls-MacBook-Air.local>
2020-10-11 15:29:18 +0530

    db struct changes

--- database/dbscript.sql
+++ database/dbscript.sql
@@ -1,6 +1,14 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Oct 11, 2020 at 09:58 AM
+-- Server version: 5.7.26
+-- PHP Version: 7.4.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
+SET time_zone = "+00:00";
 
 --
 -- Database: `fitninja`
@@ -18,7 +26,7 @@ CREATE TABLE `pwdreset` (
   `pwdResetId` int(11) NOT NULL,
   `pwdResetEmail` text NOT NULL,
   `pwdResetSelector` text NOT NULL,
-  `pwdResetToken` longtext DEFAULT NULL,
+  `pwdResetToken` longtext,
   `pwdResetExpires` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -31,7 +39,7 @@ CREATE TABLE `pwdreset` (
 CREATE TABLE `users` (
   `uid` int(11) NOT NULL,
   `username` tinytext NOT NULL,
-  `user_type` tinytext NOT NULL,
+  `user_type_id` int(11) NOT NULL,
   `email` tinytext NOT NULL,
   `password` longtext NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -40,11 +48,62 @@ CREATE TABLE `users` (
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`uid`, `username`, `user_type`, `email`, `password`) VALUES
-(1, 'mithuldas', 'C', 'mithuldas@gmail.com', '$2y$10$8zvs2dxsfiMCH6DqxF.jwee27r0Mqubs6jE4tXiwuYXySvqN8GxS.'),
-(4, '122', 'C', '122@gmail.com', '$2y$10$eRglmSLZVH1QWcn699vscuCR60GSvZ1m5ZZtxJUHfjb1e6iFh3.T2'),
-(5, '1234', 'C', '1234@hi.com', '$2y$10$zaeK.5YZX33/nMVsa4Nff.2S4unN2GIEtDfRz.cLmcom./tNg5Tg2'),
-(6, 'megha', 'C', 'meghagupta11@gmail.com', '$2y$10$GTH5W/sCPEdVMbYgibbIUe8CaJURI7H5n2B3l3W37A6w57XNwkFTO');
+INSERT INTO `users` (`uid`, `username`, `user_type_id`, `email`, `password`) VALUES
+(1, 'mithuldas', 2, 'mithuldas@gmail.com', '$2y$10$fLFeKWxVxrBnjo/vlpYNaOzav4ro1PgZRpgCq2sG3SmAmr1E3lUJe'),
+(2, 'test1234', 2, 'mithuldas@gmail.com', '$2y$10$7rcyYBkf1FKQxLhbKcgBwOsPxiumGrDXftMV047OacAhPzY7dNfyW');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_attribute_definitions`
+--
+
+CREATE TABLE `user_attribute_definitions` (
+  `attribute_id` int(11) NOT NULL,
+  `attribute_name` varchar(64) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user_attribute_definitions`
+--
+
+INSERT INTO `user_attribute_definitions` (`attribute_id`, `attribute_name`) VALUES
+(3, 'city'),
+(1, 'first_name'),
+(2, 'last_name'),
+(4, 'phone_number');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_attribute_values`
+--
+
+CREATE TABLE `user_attribute_values` (
+  `uid` int(11) NOT NULL,
+  `attribute_id` int(11) NOT NULL,
+  `attribute_value` tinytext NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_types`
+--
+
+CREATE TABLE `user_types` (
+  `user_type_id` int(11) NOT NULL,
+  `user_type_desc` tinytext NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user_types`
+--
+
+INSERT INTO `user_types` (`user_type_id`, `user_type_desc`) VALUES
+(1, 'Trainer'),
+(2, 'Trainee'),
+(3, 'Admin');
 
 --
 -- Indexes for dumped tables
@@ -60,7 +119,28 @@ ALTER TABLE `pwdreset`
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`uid`);
+  ADD PRIMARY KEY (`uid`),
+  ADD KEY `fk_users_user_types.user_type_id` (`user_type_id`);
+
+--
+-- Indexes for table `user_attribute_definitions`
+--
+ALTER TABLE `user_attribute_definitions`
+  ADD PRIMARY KEY (`attribute_id`),
+  ADD UNIQUE KEY `attribute_ame` (`attribute_name`);
+
+--
+-- Indexes for table `user_attribute_values`
+--
+ALTER TABLE `user_attribute_values`
+  ADD PRIMARY KEY (`uid`,`attribute_id`),
+  ADD KEY `attribute_id` (`attribute_id`);
+
+--
+-- Indexes for table `user_types`
+--
+ALTER TABLE `user_types`
+  ADD PRIMARY KEY (`user_type_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -70,11 +150,39 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `pwdreset`
 --
 ALTER TABLE `pwdreset`
-  MODIFY `pwdResetId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+  MODIFY `pwdResetId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `uid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
-COMMIT;
+  MODIFY `uid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `user_attribute_definitions`
+--
+ALTER TABLE `user_attribute_definitions`
+  MODIFY `attribute_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `user_types`
+--
+ALTER TABLE `user_types`
+  MODIFY `user_type_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `users`
+--
+ALTER TABLE `users`
+  ADD CONSTRAINT `fk_users_user_types.user_type_id` FOREIGN KEY (`user_type_id`) REFERENCES `user_types` (`user_type_id`);
+
+--
+-- Constraints for table `user_attribute_values`
+--
+ALTER TABLE `user_attribute_values`
+  ADD CONSTRAINT `user_attribute_values_ibfk_1` FOREIGN KEY (`attribute_id`) REFERENCES `user_attribute_definitions` (`attribute_id`),
+  ADD CONSTRAINT `user_attribute_values_ibfk_2` FOREIGN KEY (`uid`) REFERENCES `users` (`uid`);

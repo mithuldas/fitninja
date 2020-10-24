57ba0d63cdbf495998416a24bc41ab80cbd7f1d2
Mangaldas <mithul.mangaldas@sabre.com>
2020-10-24 15:43:47 +0530

    wrong file name for css fix

--- header.php
+++ header.php
@@ -9,7 +9,7 @@
 
   <!-- Bootstrap sources -->
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootswatch/4.5.2/united/bootstrap.min.css">
-  <link href="css/test.css" rel="stylesheet">
+  <link href="css/adhoc.css" rel="stylesheet">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"

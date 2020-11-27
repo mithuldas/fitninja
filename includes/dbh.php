91e3aa611d8354bada0dc3181c8cc9f9acdd4011
Mithuldas <mithuldasmail.com>
2020-11-27 19:38:33 +0530

    dbh bug

--- includes/dbh.php
+++ includes/dbh.php
@@ -5,15 +5,12 @@ $dbusername="root";
 $dbname="funinja";
 
 if(isset($_SERVER['HTTP_HOST']) and $_SERVER['HTTP_HOST']=="localhost"){ // Sabre windows laptop
-
   $dbpassword="";
-  $dbname="funinja";
-
 } else { // AWS
   $dbpassword="Aeh2kpst1!";
 }
 
-if($_SERVER['DOCUMENT_ROOT']="/Users/mithulmangaldas/funinja"){ // MAMP Mac
+if($_SERVER['DOCUMENT_ROOT']=="/Users/mithulmangaldas/funinja"){ // MAMP Mac
   $servername='127.0.0.1';
   $dbpassword='root';
 }
